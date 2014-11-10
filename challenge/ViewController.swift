a5c531697025d281db76ce23f01e9779bd12fbba
Andy Bartholomew <bart@apartmentlist.com>
2014-11-09 21:59:24 -0800

    first commit

--- challenge/ViewController.swift
+++ challenge/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomLeft: UILabel!
+    @IBOutlet weak var topRight: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var solutionLabel: UILabel!
+    @IBOutlet weak var gotItButton: UIButton!
+
+    var numbers: [Int]!
+    var solution: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewCard()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func startNewCard() {
+        generateCard()
+        let spaces = [topLeft, bottomLeft, topRight, bottomRight]
+        for (index, num) in enumerate(self.numbers) {
+            spaces[index].text = "\(num)"
+        }
     }
 
+    func randomNumber() -> Int {
+        return (random() % 9) + 1
+    }
+
+    func generateCard() {
+        self.numbers = [randomNumber(), randomNumber(), randomNumber(), randomNumber()]
+        self.solution = "whatever"
+    }
+
+    @IBAction func gotItPressed(sender: UIButton) {
+        // Display solution
+        self.solutionLabel.hidden = false
+        self.solutionLabel.text = self.solution!
+
+        self.gotItButton.hidden = true
+        self.startButton.hidden = false
+    }
+
+    @IBAction func startPressed(sender: UIButton) {
+        self.solutionLabel.hidden = true
+        self.gotItButton.hidden = false
+        self.startButton.hidden = true
+        startNewCard()
+    }
 
 }
 
